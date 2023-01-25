05de983ebee3d4f838950cedc973e5203c95ff75
Victor Henrique <victorhenriqu3@outlook.com>
2023-01-25 11:41:10 -0400

    style: add style-guide with Styled-Components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;900&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ src/styles/global.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  :root {
+    --MainColor: #0F52BA;
+    --BGColor: #f9f9f9;
+    --BGCard: #ffffff;
+    --CloseColor: #000000 ;
+    --BorderColor: rgba(0, 0, 0, 0.14)
+    --BorderInput: #BFBFBF;
+    --BGPriceColor: #373737;
+    --TextColor: #2C2C2C;
+  } 
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  #root {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 0 20px 50px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
